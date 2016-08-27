18a4474a5a57800fbafb658bcffacb6b16f8dce7
JET <jovitej@gmail.com>
2016-08-27 21:48:09 +0800

    First commit

--- LD36/Assets/Scripts/ShipController.cs
+++ LD36/Assets/Scripts/ShipController.cs
@@ -5,9 +5,11 @@ public class ShipController : MonoBehaviour {
 
   public float moveSpeed, moveCap;
   private Rigidbody2D rigidbody;
+  public float speedOfPicked;
+  private GameObject picked;
 
-	// Use this for initialization
-	void Start () {
+  // Use this for initialization
+  void Start () {
     rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
@@ -29,7 +31,45 @@ public class ShipController : MonoBehaviour {
     rigidbody.AddForce(new Vector3(moveDir * moveSpeed * Time.deltaTime, 0.0f, 0.0f));
     // Clamp ship's movement
     rigidbody.velocity = new Vector2(Mathf.Clamp(rigidbody.velocity.x, -moveCap, moveCap), 0.0f);
+
+    UpdatePickup();
 	}
+
+  void UpdatePickup()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      if (picked == null)
+      {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up); // Get it to ignore certain things
+        if (hit.collider != null)
+        {
+          if (hit.collider.tag == "Pickable") // Change to tag later?
+          {
+            print("Picked: " + hit.collider.name);
+            picked = GameObject.Find(hit.collider.name);
+            picked.GetComponent<Rigidbody2D>().gravityScale = 0;
+          }
+        }
+      }
+      else
+      {
+        picked.GetComponent<Rigidbody2D>().gravityScale = 1;
+        picked = null;
+      }
+    }
+
+    // Update the picked object
+    if (picked != null)
+    {
+      float offsetFromShip = 4;
+      Vector3 v;
+      Vector3 targetPosition = transform.position + (offsetFromShip * (-Vector3.up));
+      v = targetPosition - picked.transform.position;
+
+      picked.GetComponent<Rigidbody2D>().AddForce(v * speedOfPicked);
+    }
+  }
 
   int AngleClamp(float clampAmt)
   {
