5a9b7dbd651e5b01771bd782beca7b677cb8fb3d
Zhiming Chen <zhi.m.ch@gmail.com>
2016-08-28 23:48:49 +0800

    Playable version 0.3 - Flappy bird style

--- LD36/Assets/Scripts/BlockBehavior.cs
+++ LD36/Assets/Scripts/BlockBehavior.cs
@@ -6,6 +6,7 @@ public class BlockBehavior : MonoBehaviour
   public bool isHortizontal, isStatic;
   private Quaternion initRot;
   private Vector3 curPos;
+  private GameObject player;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,8 @@ public class BlockBehavior : MonoBehaviour
     else transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
 
     initRot = transform.rotation;
-	}
+    player = GameObject.FindGameObjectWithTag("Player");
+  }
 
 	// Update is called once per frame
 	void Update () {
@@ -31,26 +33,33 @@ public class BlockBehavior : MonoBehaviour
   //  isStatic = !isStatic;
   //}
 
-  //void OnCollisionEnter2D(Collision2D col)
-  //{
-  //  print("saving curPos " + col.gameObject.tag);
-  //  if (col.gameObject.tag == "Pickable")
-  //  {
-  //    BlockBehavior blockScript = col.gameObject.GetComponent<BlockBehavior>();
-  //    if (!isStatic && blockScript.isStatic)
-  //    {
-  //      curPos = transform.position;
-  //      isStatic = true;
-  //    }
-  //  }
-  //  else if (col.gameObject.tag == "Floor")
-  //  {
-  //    if (!isStatic) 
-  //    {
-  //      curPos = transform.position;
-  //      isStatic = true;
-  //    }
-  //  }
-  //}
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    Collider2D col = collision.collider;
+    Vector3 contactPoint = collision.contacts[0].point;
+    Vector3 center = GetComponent<Collider2D>().bounds.center;
+    bool top = contactPoint.y > center.y;
+
+    if (!top) player.GetComponent<BeamController>().UndoPicked();
+
+    //print("saving curPos " + col.gameObject.tag);
+    //if (col.gameObject.tag == "Pickable")
+    //{
+    //  BlockBehavior blockScript = col.gameObject.GetComponent<BlockBehavior>();
+    //  if (!isStatic && blockScript.isStatic)
+    //  {
+    //    curPos = transform.position;
+    //    isStatic = true;
+    //  }
+    //}
+    //else if (col.gameObject.tag == "Floor")
+    //{
+    //  if (!isStatic)
+    //  {
+    //    curPos = transform.position;
+    //    isStatic = true;
+    //  }
+    //}
+  }
 
 }
